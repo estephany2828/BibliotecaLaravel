8ebe498a90d1a603f6ace5364eade6359bba3c38
estephany2828 <ejargoty@unicauca.edu.co>
2019-07-25 00:43:48 -0500

    creada bd users, bd reserve, bd book falta categoria como array o json modificar

---
+++ database/migrations/2015_10_12_000000_create_reserves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('iduser');
+            $table->string('book');
+            $table->integer('selectedReserva');
+            $table->date('dateReserva');
+            $table->date('dateEntrega');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserves');
+    }
+}
